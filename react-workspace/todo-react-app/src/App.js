4db8f657790a77f6ce8969deaf445a9a2be333c1
jihoonk-1116 <jihoonk1116@gmail.com>
2021-12-14 18:07:18 -0500

    Implemented Mock Todo list using Props constructor & State

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -3,12 +3,25 @@ import Todo from './Todo';
 import './App.css';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    //converted item -> items list
+    this.state = {
+      items:[
+        {id:"0", title:"Hello World 1", done : true},
+        {id:"1", title:"Hello World 2", done : false},
+      ],
+    };
+  }
+
   render(){
-    return(
-      <div className="App">
-        <Todo/>
-      </div>
-    );
+    //Use map function in JS to create component with the items list
+    var todoItems = this.state.items.map((item,idx)=>(
+      <Todo item = {item} key ={item.id} /> //JSX
+    ));
+    
+    //return the generated component
+    return <div className="App">{todoItems}</div>
   }
 }
 
